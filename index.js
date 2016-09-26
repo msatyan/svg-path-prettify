4081fb5221a6badd83233738a7dad88395172942
robertjanes <robertjanes2@gmail.com>
2016-09-26 10:29:06 +1000

    Removed non-essential comments

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-function prettifyPath(path) {
+function svgPathPrettify(path) {
   for (let l = 0; l < path.length; l += 1 ) {
-    // If a letter
     if (!parseInt(path[l]) && checkIfPunc(path[l]) && path[l] !== '0') {
-      // BEFORE: If no space before (don't add space before if first)
       if (path[l - 1] !== ' ' && l !== 0 ) {
         path = path.substring(0, l) + ' ' + path.substring(l);
-        l += 1; // Increase counter for after check
+        l += 1;
       }
-      // AFTER: If no space after (don't add space after if last)
-      if (path[l + 1] !== ' ' && l !== path.length - 1) path = path.substring(0, l + 1) + ' ' + path.substring(l + 1);
+      if (path[l + 1] !== ' ' && l !== path.length - 1) {
+        path = path.substring(0, l + 1) + ' ' + path.substring(l + 1);
+      }
     }
   }
   return path;
@@ -22,4 +21,4 @@ function checkIfPunc(char) {
     : false;
 }
 
-module.exports = prettifyPath;
+module.exports = svgPathPrettify;
